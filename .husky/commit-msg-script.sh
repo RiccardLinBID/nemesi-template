a2d9cab420930226233cf238cc1cd9a67fe1567d
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-02 10:41:26 +0200

    fix: message on msg script commit type

--- .husky/commit-msg-script.sh
+++ .husky/commit-msg-script.sh
@@ -13,10 +13,11 @@ MSG=$(cat "$COMMIT_MSG_FILE")
 ALLOWED_TYPES="(\[add\]|\[chance\]|\[update\]|\[fix\]|\[delete\]|\[refactor\]|\[perf\]|feat|fix|chore|refactor|perf|docs|test|style|ci|build|revert|hotfix)(\([^)]+\))?(!)?:?"
 
 if ! echo "$MSG" | grep -qE "^$ALLOWED_TYPES"; then
-  echo "Errore: commit non conforme. Usa uno dei tipi consentiti:"
+  echo "Errore: commit non conforme. Usa uno dei tipi consentiti"
   echo "[add], [chance], [update], [fix], [delete], [refactor], [perf], feat:, fix:, chore:, refactor:, perf:, docs:, test:, style:, ci:, build:, revert:, hotfix:"
   exit 1
 fi
+
 
 # ---- Conversione dei tipi in convenzione standard (feat:, fix:, ecc.) ----
 unameOut="$(uname -s 2>/dev/null || echo Unknown)"
