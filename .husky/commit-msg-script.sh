d3728231d627af8a2362ad0167da1185b3a1e26f
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-02 10:00:50 +0200

    feat: nuova funzionalità

---
+++ .husky/commit-msg-script.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+COMMIT_MSG_FILE="$1"
+
+# Controlla che il file esista
+if [ -f "$COMMIT_MSG_FILE" ]; then
+  # Windows-friendly / Linux / macOS
+  unameOut="$(uname -s 2>/dev/null || echo Unknown)"
+  case "${unameOut}" in
+    Darwin*) sed -i '' 's/^add:/feat:/g' "$COMMIT_MSG_FILE" ;; # macOS
+    *)       sed -i 's/^add:/feat:/g' "$COMMIT_MSG_FILE" ;;     # Linux & Windows (Git Bash)
+  esac
+fi
