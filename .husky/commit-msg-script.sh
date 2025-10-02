0ad6b036e37c901d66a189ea87214bf47627ee93
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-02 10:18:34 +0200

    chore: commit-msg script

--- .husky/commit-msg-script.sh
+++ .husky/commit-msg-script.sh
@@ -7,7 +7,19 @@ if [ -f "$COMMIT_MSG_FILE" ]; then
   # Windows-friendly / Linux / macOS
   unameOut="$(uname -s 2>/dev/null || echo Unknown)"
   case "${unameOut}" in
-    Darwin*) sed -i '' 's/^\[add\]/feat:/g' "$COMMIT_MSG_FILE" ;; # macOS
-    *)       sed -i 's/^\[add\]/feat:/g' "$COMMIT_MSG_FILE" ;;     # Linux & Windows (Git Bash)
+    Darwin*)
+      sed -i '' \
+        -e 's/^\[add\]/feat:/g' \
+        -e 's/^\[fix\]/fix:/g' \
+        -e 's/^\[update\]/chore:/g' \
+        "$COMMIT_MSG_FILE"
+      ;;
+    *)
+      sed -i \
+        -e 's/^\[add\]/feat:/g' \
+        -e 's/^\[fix\]/fix:/g' \
+        -e 's/^\[update\]/chore:/g' \
+        "$COMMIT_MSG_FILE"
+      ;;
   esac
 fi
