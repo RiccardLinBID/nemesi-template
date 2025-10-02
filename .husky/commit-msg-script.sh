53ad272854e9f8ed755e21651c382b7cbff32662
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-02 10:03:11 +0200

    add: nuova funzionalità

--- .husky/commit-msg-script.sh
+++ .husky/commit-msg-script.sh
@@ -7,7 +7,25 @@ if [ -f "$COMMIT_MSG_FILE" ]; then
   # Windows-friendly / Linux / macOS
   unameOut="$(uname -s 2>/dev/null || echo Unknown)"
   case "${unameOut}" in
-    Darwin*) sed -i '' 's/^add:/feat:/g' "$COMMIT_MSG_FILE" ;; # macOS
-    *)       sed -i 's/^add:/feat:/g' "$COMMIT_MSG_FILE" ;;     # Linux & Windows (Git Bash)
+    Darwin*)
+      # macOS sed
+      sed -i '' \
+        -e 's/^\[add\]:/feat:/g' \
+        -e 's/^\[chance\]:/chore:/g' \
+        -e 's/^\[update\]:/chore:/g' \
+        -e 's/^\[fix\]:/fix:/g' \
+        -e 's/^\[delete\]:/BREAKING CHANGE:/g' \
+        "$COMMIT_MSG_FILE"
+      ;;
+    *)
+      # Linux & Windows (Git Bash)
+      sed -i \
+        -e 's/^\[add\]:/feat:/g' \
+        -e 's/^\[chance\]:/chore:/g' \
+        -e 's/^\[update\]:/chore:/g' \
+        -e 's/^\[fix\]:/fix:/g' \
+        -e 's/^\[delete\]:/BREAKING CHANGE:/g' \
+        "$COMMIT_MSG_FILE"
+      ;;
   esac
 fi
