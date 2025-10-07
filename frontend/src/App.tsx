30009285a9187c84b4251a6d95160e164d5fd5d1
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-07 11:43:51 +0200

    fix: merge changes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,9 +3,6 @@ import About from "./pages/About";
 import Home from "./pages/Home";
 import "./App.css";
 import Login from "./pages/Login";
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 
 export default function App() {
