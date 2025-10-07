74cc48094bbfa147c75b22a7d7cfff3924ba3728
Riccardo Lin <riccardo.lin@bidcompany.it>
2025-10-07 10:23:58 +0200

    chore: setup sw

--- frontend/dev-dist/sw.js
+++ frontend/dev-dist/sw.js
@@ -85,7 +85,7 @@ define(['./workbox-99d8380f'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.ts0quja3tso"
+    "revision": "0.o6820n7nuk"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
